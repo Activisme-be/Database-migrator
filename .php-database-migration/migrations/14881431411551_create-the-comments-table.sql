de57d1466b9107b7d99e041c67651315506c41da
Tim Joosten <Tjoosten3@gmail.com>
2017-02-26 22:08:51 +0100

    Implement comments table

---
+++ .php-database-migration/migrations/14881431411551_create-the-comments-table.sql
@@ -0,0 +1,14 @@
+-- // Create the comments table
+-- Migration SQL that makes the change goes here.
+CREATE TABLE IF NOT EXISTS comments (
+	PRIMARY KEY (id),
+	id 		          INT 			     AUTO_INCREMENT,
+	comments          TEXT               DEFAULT NULL,
+    updated_at        TIMESTAMP NULL     DEFAULT NULL,
+    created_at        TIMESTAMP NULL     DEFAULT NULL,
+    deleted_at        TIMESTAMP NULL     DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+-- @UNDO
+-- SQL to undo the change goes here.
+DROP TABLE IF EXISTS comments;
